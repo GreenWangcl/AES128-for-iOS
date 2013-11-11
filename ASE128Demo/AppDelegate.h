5cb9fd747e7f7c55ad2ecbca9487700d9b0c924b
zhenghaishu <zhenghaishu@youku.com>
2013-11-11 16:11:22 +0800

    Initial Commit

---
+++ ASE128Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ASE128Demo
+//
+//  Created by zhenghaishu on 11/11/13.
+//  Copyright (c) 2013 zhenghaishu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASE128DemoTests/ASE128DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ASE128DemoTests.m
+//  ASE128DemoTests
+//
+//  Created by zhenghaishu on 11/11/13.
+//  Copyright (c) 2013 zhenghaishu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASE128DemoTests : XCTestCase
+
+@end
+
+@implementation ASE128DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
